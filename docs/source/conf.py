83aa790af92e77e472e9533728ba5d6fd4974fe3
Anguelos Nicolaou <anguelos.nicolaou@gmail.com>
2020-12-31 02:20:54 +0100

    added path in  docs/source/setup.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,9 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
 # sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
 current_path = os.path.abspath(os.path.join(__file__, "..", "..", ".."))
 sys.path.append(current_path)
 
